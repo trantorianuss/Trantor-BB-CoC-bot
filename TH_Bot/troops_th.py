9339bdf6ba70e70c9fbb3779b3aef55bde196f1d
Trantorianuss <trantorianuss@proton.me>
2026-08-22 11:28:18 +0100

    Refactor troop deployment configuration: update edge zone coordinates and disable multitap for troop slots

--- TH_Bot/troops_th.py
+++ TH_Bot/troops_th.py
@@ -2,14 +2,14 @@
 
 # (slot, number of troops, deployment zone, delay, use_multitap)
 DEPLOY_SEQUENCE = [
-    (1, 8, "edge",   0,   True),
-    (2, 8, "edge",   0,   True),
+    (1, 8, "edge",   0,   False),
+    (2, 8, "edge",   0,   False),
     (4,  1, "edge",   0,   False),
     (5,  1, "edge",   0, False),
     (6,  1, "edge",   0, False),
     (7,  1, "edge",   0, False),
-    (8, 6, "random", 0,   True),
-    (1, 1, "random", 0,   True),
+    (8, 6, "random", 0,   False),
+    (1, 1, "random", 0,   False),
     (4,  0, "edge",   0,   False),
     (5,  0, "edge",   0, False),
     (6,  0, "edge",   0, False),
