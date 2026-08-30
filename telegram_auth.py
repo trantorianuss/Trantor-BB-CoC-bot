6155fda7ed71d262ba8b7f7b77993026c437912f
Trantorianuss <trantorianuss@proton.me>
2026-08-31 00:25:58 +0100

    Stop tracking Telegram credentials

--- telegram_auth.py
+++
@@ -1,7 +0,0 @@
-"""Telegram bot credentials.
-
-This file must NOT be committed to Git.
-"""
-
-BOT_TOKEN = "8679234621:AAFbEXpAeoja6T3jj7hEPL5orcv9CKUfCJk"
-CHAT_ID = 5498054333  # Replace with your Telegram chat ID.
