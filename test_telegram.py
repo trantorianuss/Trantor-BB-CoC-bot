1cad199b6faa3b2bc1265b0da87a201b63eaaf39
Trantorianuss <trantorianuss@proton.me>
2026-09-18 18:57:52 +0100

    Remove unused test files for Telegram and Tkinter components

--- test_telegram.py
+++
@@ -1,8 +0,0 @@
-"""Simple Telegram connectivity test."""
-
-from telegram import send_message
-
-
-if __name__ == "__main__":
-    send_message("Hola Mundo")
-    print("Mensaje enviado a Telegram.")
